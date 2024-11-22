0935708c9c7abd0454b9176e3b9f08bf2c005f3a
Joyce Kudumthanamkuzhy <joyce.kudumthanamkuzhy@admiral.at>
2024-11-22 18:01:16 +0100

    level 1

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
+from typing import Union, List
 # This is a sample Python script.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Hallo THOOOOOOOOMAS!!!!
-# Hallo Lukas!!!
-# Hallo Thomas!
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
-def get_smallest_amount_not_single_coin()
+
 
 
 # Press the green button in the gutter to run the script.
--- world.py
+++ world.py
@@ -1,6 +1,7 @@
 import csv, os
+from typing import Union, List, Set
 
-LEVEL = 1
+LEVEL = 2
 INPUT_DIR = os.getcwd() + "\\input\\level{0}".format(LEVEL)
 OUTPUT_DIR = os.getcwd() + "\\output\\level{0}".format(LEVEL)
 
@@ -17,15 +18,24 @@ for filename in listOfFiles:
 
         n_currencies = int(next(csv_reader)[0])
         n_coins = int(next(csv_reader)[0])
+        n_amounts = int(next(csv_reader)[0])
 
         for _ in range(n_currencies):
-            # read in the lawn
             row = next(csv_reader)
             coins = [int(x) for x in row]
-
-
+            # min amount not creatable with one coin
+            results.append(min([x for x in range(1, max(coins)+2) if x not in coins]))
 
     with open(os.path.join(OUTPUT_DIR, "level{}-{}.out".format(LEVEL, filename[-4])), 'w+') as f:
         for result in results:
-            # f.write(result + "\n")
+            f.write(str(result) + "\n")
             print(result)
+
+def get_smallest_amount_not_single_coin(coins: List[int]):
+    it = 1
+    while True:
+        if not it in coins:
+            return it
+        else:
+            it = it + 1
+
